d9c3149a5a0691ecbff29deafb265726205d0b38
Justin <nwanyajustin@gmail.com>
2019-03-31 10:36:12 +0100

    using radium for css styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,100 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Radium, {StyleRoot} from 'radium';
+import Person from './Person/Person';
 class App extends Component {
+
+  state = {
+    persons: [
+      {id:'76227378', name: "Justin", age: 29},
+      {id:'92923298', name: "Chioma", age: 27},
+      {id:'02999491', name: "Uchenna", age: 31}
+    ],
+    showPersons: false
+  };
+
+
+  nameChangedHandler = (event, id) => {
+     const personIndex = this.state.persons.findIndex(p => p.id  === id);
+     const person = {...this.state.persons[personIndex]};
+     person.name = event.target.value;
+     const persons = [...this.state.persons];
+     persons[personIndex] = person;
+    this.setState({persons: persons});
+  };
+
+
+  togglePersonHandler = () => {
+    const newShowPersonState = !this.state.showPersons;
+    this.setState({showPersons: newShowPersonState});
+  };
+
+  deletePersonHandler = (index) => {
+    console.log('delete person called');
+    // const persons = this.state.persons.slice();
+     const persons = [...this.state.persons];
+     persons.splice(index, 1);
+     this.setState({persons: persons});
+  };
+
   render() {
+   // return React.createElement('div', {className: 'App'},
+      //  React.createElement('h1', null, 'This is working'));
+
+     const style = {
+       backgroundColor: 'green',
+       color: 'white',
+       font: 'inherit',
+       border: '1px solid blue',
+       padding: '8px',
+       cursor: 'pointer',
+         ':hover': {
+           backgroundColor: 'lightgreen',
+             color: 'black',
+         }
+     };
+
+     let persons = null;
+     if(this.state.showPersons){
+       persons = (
+           <div>
+             {this.state.persons.map((person, index) => {
+               return <Person key={person.id}
+                              name={person.name}
+                              age={person.age}
+                              clicked={() => this.deletePersonHandler(index)}
+                              changed={(event) => this.nameChangedHandler(event, person.id)}
+               />
+             })}
+           </div>
+       )
+         style.backgroundColor = 'red';
+         style[':hover'] = {
+             backgroundColor: 'salmon',
+             color: 'black'
+         }
+     }
+
+     let classes = [];
+     if(this.state.persons.length <= 2){
+         classes.push('red')
+     }
+      if(this.state.persons.length <= 1){
+          classes.push('bold')
+      }
+
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <StyleRoot>
+            <div className="App">
+                <h1 className={classes.join(' ')}>Hi, I am Nwanya Justin Tochukwu</h1>
+                <button style={style} onClick={this.togglePersonHandler}>Toggle Persons</button>
+                {persons}
+            </div>
+        </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
